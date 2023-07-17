9480f753a83da54bfc124ae434440e251af7af66
Mike Hamilton <mikehamil10@users.noreply.github.com>
2023-07-17 21:04:50 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,10 @@
+function skillsMember() {
+    var member = document.getElementById("member");
+    var skills = document.getElementById("skills");
+    var memberNav = document.getElementById("memberNav");
+    var skillsNav = document.getElementById("skillsNav");
+    member.style.display = "block";
+    skills.style.display = "none";
+    memberNav.style.backgroundColor = "#f1f1f1";
+    skillsNav.style.backgroundColor = "#ccc";
+}
